7615c3bff68028f72bea5dcbcba4f14c99f0ca97
jamkwokurt <jamkowk@gmail.com>
2021-11-25 19:41:44 +1300

    center alignment

--- index.php
+++ index.php
@@ -7,8 +7,7 @@ require('db.php');
 <meta charset="utf-8">
     <title>Home Page</title>
     <link rel="stylesheet" href="css/style.css"/>
-    <!-- Bootstrap  -->
-    <link rel="stylesheet" href="css/bootstrap.css">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body id="main-container">
